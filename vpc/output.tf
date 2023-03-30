775e83ddda83cdc668fbfac773fc92f3ea348388
Jakshylyk8890 <123434138+Jakshylyk8890@users.noreply.github.com>
2023-03-30 23:57:58 +0600

    Update output.tf

--- vpc/output.tf
+++ vpc/output.tf
@@ -7,11 +7,6 @@ output "aws_subnet" {
   value = aws_subnet.pub1.*.id
   description = "output of public subnet id" 
 }
-output "private_subnets" {
-  value = aws_subnet.priv1.*.id
-  description = "output of private subnet id" 
-  
-}
 output "sg-for-bastion-host" {
   value = aws_security_group.js-sg.id
   description = "output of security group" 
