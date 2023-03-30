3c1705e03f96fa4ce7c18cb35750531e890de5a2
Jakshylyk8890 <123434138+Jakshylyk8890@users.noreply.github.com>
2023-03-31 00:16:30 +0600

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -1,3 +1,91 @@
+# 𝑽𝑷𝑪
+# 𝑃𝑢𝑏𝑙𝑖𝑐 𝑆𝑢𝑏𝑛𝑒𝑡𝑠 𝑤𝑖𝑡ℎ 𝑐𝑜𝑛𝑓𝑖𝑔𝑢𝑟𝑒𝑑 𝑅𝑜𝑢𝑡𝑒 𝑇𝑎𝑏𝑙𝑒𝑠 (𝐼𝐺𝑊)
+# 𝑃𝑟𝑖𝑣𝑎𝑡𝑒 𝑆𝑢𝑏𝑛𝑒𝑡𝑠 𝑤𝑖𝑡ℎ 𝑐𝑜𝑛𝑓𝑖𝑔𝑢𝑟𝑒𝑑 𝑅𝑜𝑢𝑡𝑒 𝑇𝑎𝑏𝑙𝑒𝑠 (𝑁𝐴𝑇-𝐺𝑊)
+#﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
+#  ᴍᴀᴅᴇ ʙʏ 𝐉𝐀𝐊𝐒𝐇𝐘𝐋𝐘𝐊 𝐀𝐒𝐇𝐘𝐑𝐌𝐀𝐌𝐀𝐓𝐎𝐕 
+#
+#       Copyright © 2023
+provider "aws" {
+  region = "us-east-1"
+}
+
+// ✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏𝐂𝐑𝐄𝐀𝐓𝐄 𝐕𝐏𝐂﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
+resource "aws_vpc" "jeks" {
+      cidr_block = var.vpc_cidr
+      tags = {
+      Name = "${var.env}-░V░P░C░"
+      enviroment = "${var.env}"
+      }
+}
+//✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏𝐂𝐑𝐄𝐀𝐓𝐄 𝐏𝐔𝐁𝐋𝐈𝐂 𝐒𝐔𝐁𝐍𝐄𝐓﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
+resource "aws_subnet" "pub1" {
+        count = length(var.public_subnet_cidr)
+        vpc_id     = aws_vpc.jeks.id
+        cidr_block = var.public_subnet_cidr[count.index]
+        availability_zone = data.aws_availability_zones.available.names[count.index]
+        map_public_ip_on_launch = true
+        tags = {
+        Name = "${var.env}-𝐒𝐔𝐁𝐍𝐄𝐓${count.index + 1}﹏﹏"
+        "kubernetes.io/role/elb" = "1"
+        enviroment = "${var.env}"
+        }
+}
+
+// ✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏𝐂𝐑𝐄𝐀𝐓𝐄 𝐈𝐍𝐓𝐄𝐑𝐍𝐄𝐓 𝐆𝐀𝐓𝐄𝐖𝐀𝐘 ﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
+resource "aws_internet_gateway" "gw" {
+      vpc_id = aws_vpc.jeks.id
+
+      tags = {
+      Name = "${var.env}-𝒊𝒏𝒕𝒆𝒓𝒏𝒆𝒕-𝒈𝒂𝒕𝒆𝒘𝒂𝒚"
+      enviroment = "${var.env}"
+      }
+}
+//✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏𝐏𝐔𝐁𝐋𝐈𝐂 𝐑𝐎𝐔𝐓𝐄 𝐓𝐀𝐁𝐋𝐄﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
+resource "aws_route_table" "rt" {
+        count = length(var.public_subnet_cidr)
+        vpc_id = aws_vpc.jeks.id
+
+  route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gw.id
+        }
+        tags = {
+        Name = "-${var.env}-ROUTE${count.index + 1}☆☆╮"
+        enviroment = "${var.env}"
+        }
+}
+//✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏𝐏𝐔𝐁𝐋𝐈𝐂 𝐑𝐎𝐔𝐓𝐄 𝐓𝐀𝐁𝐋𝐄 𝐀𝐒𝐒𝐎𝐂𝐈𝐀𝐓𝐈𝐎𝐍﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
+resource "aws_route_table_association" "a" {
+        count = length(var.public_subnet_cidr)
+        subnet_id      = aws_subnet.pub1.*.id[count.index]
+        route_table_id = aws_route_table.rt.*.id[count.index]
+}
+
+
+//✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏𝐂𝐑𝐄𝐀𝐓𝐄 𝐒𝐄𝐂𝐔𝐑𝐈𝐓𝐘 𝐆𝐑𝐎𝐔𝐏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
+resource "aws_security_group" "js-sg" {
+        name        = "${var.env}-security_group"
+        vpc_id      = aws_vpc.jeks.id
+  dynamic "ingress" {
+      for_each = var.ingress 
+  content {
+        from_port = ingress.value
+        to_port = ingress.value
+        protocol = "-1"
+        cidr_blocks =  ["0.0.0.0/0"]
+        }
+  }
+  egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+   }
+}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 //✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏𝐂𝐑𝐄𝐀𝐓𝐄 𝐄𝐊𝐒 𝐂𝐋𝐔𝐒𝐓𝐄𝐑﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 resource "aws_eks_cluster" "cluster" {
     version  = "1.24"
